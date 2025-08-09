f4392c59253d0853cedeb26a44c90c8ad777fe11
thecodingmontana <dev.humblechris254@gmail.com>
2025-08-09 13:00:33 +0300

    feat(app): add eslint

---
+++ src/routes/__root.tsx
@@ -0,0 +1,81 @@
+/// <reference types="vite/client" />
+import type { QueryClient } from "@tanstack/react-query";
+import {
+  createRootRouteWithContext,
+  HeadContent,
+  Outlet,
+  Scripts,
+} from "@tanstack/react-router";
+
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+
+import { getUser } from "~/lib/auth/functions/getUser";
+import appCss from "~/styles.css?url";
+
+import { ThemeProvider } from "~/components/theme-provider";
+import { Toaster } from "~/components/ui/sonner";
+
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient;
+  user: Awaited<ReturnType<typeof getUser>>;
+}>()({
+  beforeLoad: async ({ context }) => {
+    const user = await context.queryClient.ensureQueryData({
+      queryKey: ["user"],
+      queryFn: ({ signal }) => getUser({ signal }),
+      revalidateIfStale: true,
+    }); // we're using react-query for caching, see router.tsx
+    return { user };
+  },
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
+        title: "React TanStarter",
+      },
+      {
+        name: "description",
+        content: "A minimal starter template for 🏝️ TanStack Start.",
+      },
+    ],
+    links: [{ rel: "stylesheet", href: appCss }],
+  }),
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <Outlet />
+    </RootDocument>
+  );
+}
+
+function RootDocument({ children }: { readonly children: React.ReactNode }) {
+  return (
+    // suppress since we're updating the "dark" class in ThemeProvider
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        <ThemeProvider>
+          {children}
+          <Toaster richColors />
+        </ThemeProvider>
+
+        <ReactQueryDevtools buttonPosition="bottom-right" />
+        <TanStackRouterDevtools position="bottom-right" />
+
+        <Scripts />
+      </body>
+    </html>
+  );
+}
