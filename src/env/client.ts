2dd1cdeca099cbfc197001de18b1346f0f8494e2
thecodingmontana <dev.humblechris254@gmail.com>
2025-08-23 05:57:19 +0300

    chore(app): save local changes

--- src/env/client.ts
+++ src/env/client.ts
@@ -5,6 +5,9 @@ export const env = createEnv({
   clientPrefix: "VITE_",
   client: {
     VITE_BASE_URL: z.url().default("http://localhost:3000"),
+    // Paystack
+    VITE_PAYSTACK_SECRET_KEY: z.string().min(1),
+    VITE_PAYSTACK_PUBLIC_KEY: z.string().min(1),
   },
   runtimeEnv: import.meta.env,
 });
